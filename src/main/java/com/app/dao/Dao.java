c72cc5e6cc04478b84a9fbdad52e10cac7a61742
uras <uras@i.ua>
2016-10-06 21:22:31 +0300

    add gitignore

--- src/main/java/com/app/dao/Dao.java
+++ src/main/java/com/app/dao/Dao.java
@@ -1,4 +1,23 @@
 package com.app.dao;
 
-public class Dao {
+import java.util.List;
+
+/**
+ * Created by Andre on 21.06.2016.
+ */
+public interface DAO {
+    public void createEmployee(String name);
+
+    public Employee getEmployye(long id);
+
+    public List<Employee> getEmployeeByName(String name);
+
+    public void updateEmployee(long id, Employee employee);
+
+    public void deleteEmployee(long id);
+
+    public void readMetadata();
+
+    public void bulkAction();
+
 }
